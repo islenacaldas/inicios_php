b53962890903fec643fed6ef1d8e69e8a3995972
IslenaCaldas <islenacaldas@gmail.com>
2026-08-18 09:53:43 -0500

    conociendo la escritura de los condicionales en php

--- ejercicio1.php
+++ ejercicio1.php
@@ -7,5 +7,20 @@
 </head>
 <body>
     <h1>hello bebis</h1>
+    <?php
+    /*$nombre = "Pedro";
+    $apellido = "Perez";
+    $edad = 25;
+
+    echo "Bienvenido " . $nombre . " " . $apellido . " Su edad es " . $edad;
+     */
+    $num1=3;
+    $num2=4;
+    $num3=5;
+    echo $num1.$num2.$num3 . "<br>";
+    echo $num3.$num1.$num2 . "<br>";
+    ?>
+   
+
 </body>
 </html>
