506d47cb37d4281c943856386c3b137c8c89374a
IslenaCaldas <islenacaldas@gmail.com>
2026-08-18 10:00:24 -0500

    mirando casos c on switch en php

--- ejercicio1.php
+++ ejercicio1.php
@@ -13,12 +13,46 @@
     $edad = 25;
 
     echo "Bienvenido " . $nombre . " " . $apellido . " Su edad es " . $edad;
-     */
+    //concatenacion de variables 
     $num1=3;
     $num2=4;
     $num3=5;
     echo $num1.$num2.$num3 . "<br>";
     echo $num3.$num1.$num2 . "<br>";
+    echo "<p>" .$num1.$num2 . "</p>";
+   // TIPO DE DATOS PARA UNA VARIABLE APARTIR DE EL "VAR_DUMP", ESTO IMPRIME EL TIPO DE DATO
+    $num1=3;
+    $num2=4;
+    $num3=5;
+    $pais= "Colombia";
+    var_dump($num1);
+    var_dump($pais)
+    //condicionales con if-else
+
+    $a=10;
+    $b=20;
+    if($a>$b){
+        echo "a es mayor que b";
+    }else{
+        echo "b es mayor que a";
+    }
+    //creacion de switch
+    */
+    echo "<p> uso de switch </p>";
+    $semaforo="verde";
+    switch($semaforo){
+        case"rojo":
+            echo "<p>Semaforo en rojo - detengase</p>";
+            break;
+        case "amarillo":
+            echo "<p>Semaforo en amarillo- preparese</p>";
+            break;
+        case "verde":
+            echo "<p>Semaforo en verde- Avance</p>";
+            break;
+        default:
+            echo "<p>color no valido</p>";
+    }
     ?>
    
 
